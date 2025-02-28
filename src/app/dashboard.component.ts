c2b36307c02fa9906c304be7739f9d6ec01676db
koushik717 <101172701+koushik717@users.noreply.github.com>
2025-02-28 14:09:19 -0500

    dashboard.component.ts

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,34 +1,32 @@
 // dashboard.component.ts
 import { Component } from '@angular/core';
-import { DashboardService } from '../../services/dashboard.service';
+import { DashboardService } from './services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  template: `
+    <div class="dashboard-grid">
+      <app-quick-stats [metrics]="metrics"></app-quick-stats>
+      <div class="main-content">
+        <div class="booking-list">
+          <div *ngFor="let booking of bookings" class="booking-card">
+            {{ booking.room }} - {{ booking.dates.join(' to ') }}
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
   metrics: any;
   bookings: any[] = [];
-  occupancyData: any[] = [];
 
-  constructor(private dashboardService: DashboardService) {}
-
-  ngOnInit() {
-    this.dashboardService.getMetrics().subscribe(data => {
-      this.metrics = data;
-      this.occupancyData = this.transformChartData(data.occupancyTrend);
+  constructor(private dashboardService: DashboardService) {
+    this.dashboardService.getData().subscribe(data => {
+      this.metrics = data.metrics;
+      this.bookings = data.bookings;
     });
-
-    this.dashboardService.getBookings().subscribe(bookings => {
-      this.bookings = bookings;
-    });
-  }
-
-  private transformChartData(rawData: any) {
-    return [{
-      name: 'Occupancy',
-      type: 'line',
-      data: rawData.map((d: any) => d.value)
-    }];
   }
 }
+
